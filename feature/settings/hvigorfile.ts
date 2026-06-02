101ef3848a091d7a0c9573b97760d55996dfef0e
halleyallenMatebook <1512538733@qq.com>
2026-06-02 23:41:22 +0800

    添加动态显示任务栏

---
+++ feature/settings/hvigorfile.ts
@@ -0,0 +1,8 @@
+import { harPlugin } from '@hadss/hmrouter-plugin/dist/Index';
+import { harTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+  system: harTasks,
+  plugins: [harPlugin()]
+  // plugins: []
+}
