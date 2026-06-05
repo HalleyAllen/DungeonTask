4a3acb3f5dfff318136190ca9766f7d543b86027
halleyallenMatebook <1512538733@qq.com>
2026-06-05 23:16:41 +0800

    调整页面跳转

--- feature/task/hvigorfile.ts
+++ feature/task/hvigorfile.ts
@@ -1,6 +1,7 @@
+import { harPlugin } from '@hadss/hmrouter-plugin/dist/Index';
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
   system: harTasks,
-  plugins: []
+  plugins: [harPlugin()]
 }
